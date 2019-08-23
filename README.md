1f60536f8be6331d1d0e0d2635bf38f3f41a989b
Yorck Ramachers <y.a.ramachers@warwick.ac.uk>
2019-08-23 13:44:55 +0100

    [tool] cleaned source code. Tests reduced to 4.

--- README.md
+++ README.md
@@ -8,7 +8,8 @@ Last updated August 22 2019
 - CMakeLists.txt
 - README.md
 - SimulationValidationTool.cxx
-- SimulationValidationTool.h
+- getopt_pp.cpp
+- getopt_pp.h
 
 ## Purpose
 
@@ -23,16 +24,16 @@ The tool is used to :
 ## Usage
 
 After builiding the tool run the following command in the build directory to use the programme.
-$ ./SimulationValidationTool <data ROOT file> <reference ROOT file to compare to>
+$ ./SimulationValidationTool -i <data ROOT file> -r <reference ROOT file to compare to>
 
 In order to generate comparison statistics the root input and reference files should contain branches with the same names.
 The output of the tool is presented in the terminal running in. 
 Some basic tests are present which compare data from input and reference files.
 
 The  statistics  generated  by  the  SimulationValidationTool  are:  Mean,  Error  on  Mean,  Maximum  Value, Minimum  Value,  Skewness,  Standard  Deviation,  
-Error  on  Standard  Deviation,  Kolmogorov-Smirnov  Test,  P-value, Chi-squared  and  number  of  degrees of freedom.
+Error  on  Standard  Deviation,  Kolmogorov-Smirnov  Test.
 
-Currently,  there are 5 example tests run by the SimulationValidationTool:
+Currently,  there are 4 example tests run by the SimulationValidationTool:
 
 1. check if input data mean lies in the range of reference data mean and one standard deviation, 
 and vice versa for file order reversed;
@@ -43,7 +44,5 @@ and vice versa for file order reversed;
 and vice versa for file order reversed;
 
 4. check if Maximum of input file is larger than Minimum of reference file, and vice versa for file order reversed;
-
-5. check if P-value is larger than 0.99.
 
 Note: To use this tool the branches have to be saved in a Tree titled "SimValidation".
--- SimulationValidationTool.cxx
+++ SimulationValidationTool.cxx
@@ -6,9 +6,8 @@
 #include "TFile.h"
 #include "TTree.h"
 #include "TH1.h"
-#include "TCanvas.h"
 #include "TBranch.h"
-#include "TMath.h"
+
 
 // Global variables
 bool hasValidReference = true;
@@ -48,11 +47,12 @@ int main(int argc, char **argv) {
   
   // Call Function
   ParseRootFile(inputFileName, refFileName);
+  return 1;
 }
 
 
 
-void ParseRootFile(string rootFileName, string refFileName) {
+void ParseRootFile(std::string rootFileName, std::string refFileName) {
   // Check the input root file can be opened and contains a tree with the right name
   std::cout<<"Processing "<<rootFileName<<std::endl;
   TFile *rootFile;
@@ -72,15 +72,12 @@ void ParseRootFile(string rootFileName, string refFileName) {
     
   // Check for a reference file
   TFile *refFile;
-  if (refFileName.length() > 0) {
-    refFile = new TFile(refFileName.c_str());
-    if (refFile->IsZombie()) {
-      std::cout<<"WARNING: No valid reference ROOT file given";
-      if (refFileName.length()>0) cout<<" Bad ROOT file: "<<refFileName;
-      std::cout <<std::endl;
-      hasValidReference = false;
-    }
+  refFile = new TFile(refFileName.c_str());
+  if (refFile->IsZombie()) {
+    std::cout << "WARNING: No valid reference ROOT file given." << std::endl;
+    hasValidReference = false;
   }
+  
   if (hasValidReference) {
     reftree = (TTree*) refFile->Get(treeName.c_str());
     // Check if it found the tree
@@ -101,32 +98,33 @@ void ParseRootFile(string rootFileName, string refFileName) {
   
   // Loop through Branches
   while( (branch=(TBranch *)briter.Next() )) {
-    string branchName=branch->GetName();
+    std::string branchName=branch->GetName();
     // Call Function
     CompareHistogram(branchName); 
   }
+  refFile.Close();
+  rootFile.Close();
 }
 
-void CompareHistogram(string branchName) {
-  int notSetVal=-9999;
+void CompareHistogram(std::string branchName) {
   int nbins=100;
-  double lowLimit=0;
-  double highLimit=notSetVal;
-  string title="";
-  bool hasReferenceBranch=hasValidReference;
+  double lowLimit = 0;
+  double highLimit = -9999;
+  std::string title="";
   
   // Check whether the reference file contains this branch
   if (hasValidReference) {
-    hasReferenceBranch=reftree->GetBranchStatus(branchName.c_str());
-    if (!hasReferenceBranch) cout<<"WARNING: branch "<<branchName<<" not found in reference file. No comparison statistics will be made for this branch"<<endl;
+    bool hasReferenceBranch = reftree->GetBranchStatus(branchName.c_str());
+    if (!hasReferenceBranch) {
+      cout<<"WARNING: branch "<<branchName<<" not found in reference file. No comparison statistics will be made for this branch"<<endl;
+      return;
+    }
   }
   
   // Create Histograms to work with from input file
-  TCanvas *c = new TCanvas (("plot_"+branchName).c_str(),("plot_"+branchName).c_str(),900,600);
-  TH1D *h;
-  h = new TH1D(("plt_"+branchName).c_str(),title.c_str(),nbins,lowLimit,highLimit);
-  if( h->GetSumw2N() == 0 )h->Sumw2();
-  tree->Draw((branchName + ">> plt_"+branchName).c_str());
+  TH1D *h = new TH1D(("plt_"+branchName).c_str(),title.c_str(),nbins,lowLimit,highLimit); // automatic limit
+  if ( h->GetSumw2N() == 0 ) h->Sumw2();
+  tree->Draw((branchName + ">> plt_"+branchName).c_str(),"","goff");
 
   // Create Histograms to work with from reference file
   TH1D *href = (TH1D*) h->Clone(); // To work with Histograms of identical size we clone h
@@ -136,36 +134,32 @@ void CompareHistogram(string branchName) {
   if( href->GetSumw2N() == 0 )href->Sumw2();
   
   //Filling
-  reftree->Draw((branchName + ">> ref_"+branchName).c_str());
-   
+  reftree->Draw((branchName + ">> ref_"+branchName).c_str(),"","goff"); 
+  
   // Normalise reference number of events to data
-  Double_t scale = (double)tree->GetEntries()/(double)reftree->GetEntries();
+  double scale = (double)tree->GetEntries()/(double)reftree->GetEntries();
   href->Scale(scale);
 
   // Calculate Comparison Statistics
-  Double_t ks = h->KolmogorovTest(href); // Kolmogorov Test
+  double ks = h->KolmogorovTest(href); // Kolmogorov Test
   
   // Input File Data
-  Double_t std = h->GetStdDev(); // Standard Deviation
-  Double_t std_error = h->GetStdDevError(); // Error on Standard Deviation
-  Double_t skew = h->GetSkewness(); // Skewness
-  Double_t mean = h->GetMean(); // Mean
-  Double_t mean_error = h->GetMeanError(); // Error on Mean
-  Double_t max = h->GetMaximum(); // Maximum
-  Double_t min = h->GetMinimum (); // Minimum  
+  double std = h->GetStdDev(); // Standard Deviation
+  double std_error = h->GetStdDevError(); // Error on Standard Deviation
+  double skew = h->GetSkewness(); // Skewness
+  double mean = h->GetMean(); // Mean
+  double mean_error = h->GetMeanError(); // Error on Mean
+  double max = h->GetMaximum(); // Maximum
+  double min = h->GetMinimum (); // Minimum  
   
   // Reference File Data
-  Double_t std_ref = href->GetStdDev(); // Standard Deviation
-  Double_t std_error_ref = href->GetStdDevError(); // Error on Standard Deviation
-  Double_t skew_ref = href->GetSkewness(); // Skewness
-  Double_t mean_ref = href->GetMean(); // Mean
-  Double_t mean_error_ref = href->GetMeanError(); // Error on Mean
-  Double_t max_ref = href->GetMaximum(); // Maxmimum
-  Double_t min_ref = href->GetMinimum (); // Minimum
-  
-  Double_t chisq;
-  Int_t ndf;
-  Double_t p_value = ChiSquared(h, href, chisq, ndf, false);
+  double std_ref = href->GetStdDev(); // Standard Deviation
+  double std_error_ref = href->GetStdDevError(); // Error on Standard Deviation
+  double skew_ref = href->GetSkewness(); // Skewness
+  double mean_ref = href->GetMean(); // Mean
+  double mean_error_ref = href->GetMeanError(); // Error on Mean
+  double max_ref = href->GetMaximum(); // Maxmimum
+  double min_ref = href->GetMinimum (); // Minimum
   
   std::cout<<"Comparing branches: "<<branchName<<std::endl;
   std::cout<<""<<std::endl;
@@ -177,46 +171,40 @@ void CompareHistogram(string branchName) {
   std::cout<<"Std: "<<std<<" ; Reference Std:"<<std_ref<<std::endl;
   std::cout<<"Std Error: "<<std_error<<" ; Reference Std Error:"<<std_error_ref<<std::endl;
   std::cout<<"Kolmogorov: "<<ks<<std::endl;
-  std::cout<<"P-value: "<<p_value<<std::endl;
-  std::cout<<"Chi-square: "<<chisq<<std::endl;
-  std::cout<<"DoF: "<<chisq/(double)ndf<<std::endl;
   std::cout<<""<<std::endl;
   
   std::cout<<"Testing branches: "<<branchName<<std::endl;
   
   // Running Tests on Comparisons
-  if (mean>(mean+std_ref) || mean<(mean-std_ref) || (std_error/std_error_ref)>std_ref || (std_error_ref/std_error)>std_ref || (std_error_ref/std_error)>std || (std_error/std_error_ref)>std || 
-  mean>(mean_ref+mean_error_ref) || mean<(mean_ref-mean_error_ref) || mean_ref>(mean+mean_error) || mean_ref<(mean-mean_error)  || max<min_ref || max_ref<min || p_value<0.99) {
-    
-    // Both means should lie within 1 std from the other mean (h compared to href and vice versa)
-    if (mean>(mean_ref+std_ref) || mean<(mean_ref-std_ref)) {
-      std::cout<<"Error: Mean outside of 1 Standard Deviation"<<std::endl;
-    }
-    
-    // Arbitrary account of the difference in std error
-    if((std_error/std_error_ref)>1.01|| (std_error_ref/std_error)>1.01 || (std_error_ref/std_error)<0.99 || (std_error/std_error_ref)<0.99) {
-      std::cout<<"Error: Standard Deviation Error to large"<<std::endl;
-    }
-    
-    // Mean values and Errors on Mean Values
-    if(mean>(mean_ref+mean_error_ref) || mean<(mean_ref-mean_error_ref) || mean_ref>(mean+mean_error) || mean_ref<(mean-mean_error)) {
-      std::cout<<"Error: Mean Value outside error bounds"<<std::endl;
-    }
-    
-    // Simple Tests on Max and Min
-    if(max<min_ref || max_ref<min) {
-      std::cout<<"Error: Max, Min reversed"<<std::endl;
-    }
-    
-    // Arbitrary P-value test
-    if(p_value<0.99) {
-      std::cout<<"Error: P-value less than 0.99"<<std::endl;
-    }
-  }
-  else {
-    std::cout<<"All Tests Passed"<<std::endl;
+  // Both means should lie within 1 std from the other mean (h compared to href and vice versa)
+  bool pass = true;
+  if (mean>(mean_ref+std_ref) || mean<(mean_ref-std_ref)) {
+    std::cout<<"Error: Mean outside of 1 Standard Deviation"<<std::endl;
+    pass = false;
   }
   
+  // Arbitrary account of the difference in std error
+  if((std_error/std_error_ref)>1.01|| (std_error_ref/std_error)>1.01 || (std_error_ref/std_error)<0.99 || (std_error/std_error_ref)<0.99) {
+    std::cout<<"Error: Standard Deviation Error to large"<<std::endl;
+    pass = false;
+  }
+  
+  // Mean values and Errors on Mean Values
+  if(mean>(mean_ref+mean_error_ref) || mean<(mean_ref-mean_error_ref) || mean_ref>(mean+mean_error) || mean_ref<(mean-mean_error)) {
+    std::cout<<"Error: Mean Value outside error bounds"<<std::endl;
+    pass = false;
+  }
+    
+  // Simple Tests on Max and Min
+  if(max<min_ref || max_ref<min) {
+    std::cout<<"Error: Max, Min reversed"<<std::endl;
+    pass = false;
+  }
+  
+  if (pass) {
+    std::cout<<"All Tests Passed"<<std::endl;
+  }
+    
   std::cout<<"---- "<<"Finished working with branches: "<<branchName<<" ----"<<std::endl;
   std::cout<<""<<std::endl;
   
@@ -224,36 +212,4 @@ void CompareHistogram(string branchName) {
   delete h;
 }
 
-
-double ChiSquared(TH1 *h1, TH1 *h2, double &chisq, int &ndf, bool isAverage) {
-    // Calculate a chi squared per degree of freedom
-    chisq=0;
-    ndf=0;
-    
-    for (int x=1; x<=h1->GetNbinsX();x++) {
-        for (int y=1; y<=h1->GetNbinsY();y++) {
-            double val1 = h1->GetBinContent(x,y);
-            double  val2 = h2->GetBinContent(x,y);
-            double err1 = h1->GetBinError(x,y);
-            double err2 = h2->GetBinError(x,y);
-            // We won't count the bin (or increase the degrees of freedom) if we don't have enough info
-            // This is the case if either uncertainty is zero or  if a value or uncertainty is not a number
-            if ((std::isnan(val1) || std::isnan(val2) || std::isnan(err1) || std::isnan(err2))) {
-                // cout<<"Insufficient information for bin  ("<<x<<","<<y<<"): do not include in chi square calculation"<<endl;
-                continue;
-            }
-            
-            if (err1 == 0 || err2 == 0) { // Should never be the case!
-                //cout<<"Insufficient information for bin  ("<<x<<","<<y<<"): do not include in chi square calculation"<<endl;
-                continue;
-            }
-            
-            ndf++;
-            double numerator = pow((val1 - val2), 2);
-            double denominator = pow(err1,2) + pow(err2,2);
-            chisq += numerator/denominator;
-        }
-    }
-    return TMath::Prob(chisq, ndf);
-}
 
